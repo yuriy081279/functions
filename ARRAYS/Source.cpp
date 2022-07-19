0137260d226a8aad25d209a42b23339eba4f032f
Юрий Лейкехман <leikehman@yandex.ru>
2022-07-19 12:55:39 +0300

    ochen bolshaya zadacha

--- ARRAYS/Source.cpp
+++ ARRAYS/Source.cpp
@@ -9,9 +9,9 @@ void SORT(int arr[], const int n);
 void Print(int arr[], const int n);
 double AVG(int arr[], const int n);
 void shiftLeft(int arr[], const int n);
-int SUM (int arr[], const int n);
-void minValueIn(int arr[], const int n);
-void maxValueIn(int arr[], const int n);
+int SUM(int arr[], const int n);
+int minValueIn(int arr[], const int n);
+int maxValueIn(int arr[], const int n);
 void shiftRight(int arr[], const int n);
 void main()
 {
@@ -24,12 +24,14 @@ void main()
 	Print(arr, n);
 	cout << "сумма всех значений массива = " << SUM(arr, n) << endl;
 	cout << "среднее арифметическое всех значений массива = " << AVG(arr, n) << endl;
-	cout << "минимальное значение из массива = "; minValueIn(arr, n); cout<<endl;
-	cout << "максимальное значение из массива = "; maxValueIn(arr, n); 
+	cout << "минимальное значение из массива = " << minValueIn(arr, n)<< endl;
+	cout << "максимальное значение из массива = " << maxValueIn(arr, n) << endl;
 	cout << endl;
 	cout << "массив со сдвигом вправо "; shiftRight(arr, n);
 	cout << endl;
 	cout << "массив со сдвигом влево "; shiftLeft(arr, n);
+
+
 }
 //Вывод массива на экран:
 void FillRand(int arr[], const int n)
@@ -46,13 +48,13 @@ void Print(int arr[], const int n)
 		cout << arr[i] << tab;
 	}
 	cout << endl;
-	
+
 }
 void SORT(int arr[], const int n)
 {
 	for (int i = 0; i < n; i++)
 	{
-		for (int j =i+1; j < n; j++)
+		for (int j = i + 1; j < n; j++)
 		{
 			if (arr[j] < arr[i])
 			{
@@ -62,7 +64,7 @@ void SORT(int arr[], const int n)
 			}
 
 		}
-	}	
+	}
 }
 int SUM(int arr[], const int n)
 {
@@ -74,23 +76,16 @@ int SUM(int arr[], const int n)
 	return x;
 }
 double AVG(int arr[], const int n)
-{
-	double x = 0;
-	double y;
-	for (int i = 0; i < n; i++)
-	{
-		x += arr[i];
-		y = x / n;
-	}
-	return y;
+{		
+	return (double) SUM(arr, n) / n;
 }
-void minValueIn(int arr[], const int n)
+int minValueIn(int arr[], const int n)
 {
-	cout << arr[0];
+	return arr[0];
 }
-void maxValueIn(int arr[], const int n)
+int maxValueIn(int arr[], const int n)
 {
-	cout<< arr[n-1];
+	return arr[n - 1];
 }
 void shiftRight(int arr[], const int n)
 {
@@ -105,22 +100,22 @@ void shiftRight(int arr[], const int n)
 		arr[n - 1] = x;
 	}
 	for (int i = 0; i < n; i++)
-	cout << arr[i]<<tab;	
+		cout << arr[i] << tab;
 }
 void shiftLeft(int arr[], const int n)
 {
 	int y = 4;
 	for (int i = 0; i < y; i++)
 	{
-		int x = arr[n-1];
-		for (int i = n-1; i > 0; i--)
+		int x = arr[n - 1];
+		for (int i = n - 1; i > 0; i--)
 		{
 			arr[i] = arr[i - 1];
 		}
 		arr[0] = x;
 	}
 	for (int i = 0; i < n; i++)
-		cout << arr[i]<<tab;
+		cout << arr[i] << tab;
 	cout << endl;
 }
 
---
+++ Arrays 2D/main.cpp
@@ -0,0 +1,482 @@
+﻿#include<iostream>
+using namespace std;
+#define tab "\t"
+const int STROKA = 3;
+const int STOLB = 4;
+
+void FillRand(int arr[STROKA][STOLB], const int STROKA, const int STOLB);
+void FillRand(double arr[STROKA][STOLB], const int STROKA, const int STOLB);
+void FillRand(char arr[STROKA][STOLB], const int STROKA, const int STOLB);
+void FillRand(float arr[STROKA][STOLB], const int STROKA, const int STOLB);
+
+void Print (int arr[STROKA][STOLB], const int STROKA, const int STOLB);
+void Print (double arr[STROKA][STOLB], const int STROKA, const int STOLB);
+void Print (char arr[STROKA][STOLB], const int STROKA, const int STOLB);
+void Print (float arr[STROKA][STOLB], const int STROKA, const int STOLB);
+
+double Sum (int arr[STROKA][STOLB], const int STROKA, const int STOLB);
+double Sum (double arr[STROKA][STOLB], const int STROKA, const int STOLB);
+char Sum (char arr[STROKA][STOLB], const int STROKA, const int STOLB);
+float Sum (float arr[STROKA][STOLB], const int STROKA, const int STOLB);
+
+double Avg (int arr[STROKA][STOLB], const int STROKA, const int STOLB);
+double Avg (double arr[STROKA][STOLB], const int STROKA, const int STOLB);
+char Avg (char arr[STROKA][STOLB], const int STROKA, const int STOLB);
+float Avg (float arr[STROKA][STOLB], const int STROKA, const int STOLB);
+
+int minValueIn(int arr[STROKA][STOLB], const int STROKA, const int STOLB);
+double minValueIn(double arr[STROKA][STOLB], const int STROKA, const int STOLB);
+char minValueIn(char arr[STROKA][STOLB], const int STROKA, const int STOLB);
+float minValueIn(float arr[STROKA][STOLB], const int STROKA, const int STOLB);
+
+int maxValueIn(int arr[STROKA][STOLB], const int STROKA, const int STOLB);
+double maxValueIn(double arr[STROKA][STOLB], const int STROKA, const int STOLB);
+char maxValueIn(char arr[STROKA][STOLB], const int STROKA, const int STOLB);
+float maxValueIn(float arr[STROKA][STOLB], const int STROKA, const int STOLB);
+//void shiftLeft (int arr[STROKA][STOLB], const int STROKA, const int STOLB);
+
+//int shiftRight();
+
+void main()
+{
+	setlocale(LC_ALL, "");
+	
+	int i_arr_2[STROKA][STOLB];
+	double i_brr_2[STROKA][STOLB];
+	char i_crr_2[STROKA][STOLB];
+	float i_drr_2[STROKA][STOLB];
+	
+	
+	FillRand(i_arr_2, STROKA, STOLB);
+	Print(i_arr_2, STROKA, STOLB);
+	cout << " Сумма элементов данного массива = " << Sum(i_arr_2, STROKA, STOLB)<<endl<<endl;	
+	cout << " Среднее арифметическое элементов этого массива = " << Avg(i_arr_2, STROKA, STOLB)<<endl<<endl;	
+	cout << " Минимальное значение в этом массиве = "<< minValueIn(i_arr_2, STROKA, STOLB)<< endl << endl;
+	cout << " Максимальное значение в этом массиве = "<< maxValueIn(i_arr_2, STROKA, STOLB)<< endl << endl;
+	
+
+	FillRand(i_brr_2, STROKA, STOLB);
+	Print(i_brr_2, STROKA, STOLB);
+	cout << " Сумма элементов данного массива = " << Sum(i_brr_2, STROKA, STOLB)<<endl<<endl;
+	cout << " Среднее арифметическое элементов этого массива = " << Avg(i_brr_2, STROKA, STOLB) << endl << endl;
+	cout << " Минимальное значение в массиве = " << minValueIn(i_brr_2, STROKA, STOLB) << endl << endl;
+	cout << " Максимальное значение в этом массиве = " << maxValueIn(i_brr_2, STROKA, STOLB) << endl << endl;
+
+	FillRand(i_crr_2, STROKA, STOLB);
+	Print(i_crr_2, STROKA, STOLB);
+	cout << " Сумма элементов данного массива = " << Sum(i_crr_2, STROKA, STOLB)<<endl<<endl;
+	cout << " Среднее арифметическое элементов этого массива = " << Avg(i_crr_2, STROKA, STOLB) << endl << endl;
+	cout << " Минимальное значение в массиве  = " << minValueIn(i_crr_2, STROKA, STOLB) << endl << endl;
+	cout << " Максимальное значение в этом массиве = " << maxValueIn(i_crr_2, STROKA, STOLB) << endl << endl;
+
+	FillRand(i_drr_2, STROKA, STOLB);
+	Print(i_drr_2, STROKA, STOLB);
+	cout << " Сумма элементов данного массива = " << Sum(i_drr_2, STROKA, STOLB) << endl << endl;
+	cout << " Среднее арифметическое элементов этого массива = " << Avg(i_drr_2, STROKA, STOLB) << endl << endl;
+	cout << " Минимальное значение в массиве = " << minValueIn(i_drr_2, STROKA, STOLB) << endl << endl;
+	cout << " Максимальное значение в этом массиве = " << maxValueIn(i_drr_2, STROKA, STOLB) << endl << endl;
+}
+
+void FillRand(int arr[STROKA][STOLB], const int STROKA, const int STOLB)
+{
+	for (int i = 0; i < STROKA; i++)
+	{
+		for (int j = 0; j < STOLB; j++)
+		{
+			arr[i][j]=rand()%100;
+		}
+	}
+}
+void FillRand(double arr[STROKA][STOLB], const int STROKA, const int STOLB)
+{	
+	for (int i = 0; i < STROKA; i++)
+	{
+		for (int j = 0; j < STOLB; j++)
+		{
+			arr[i][j]=rand()%1000;
+			arr[i][j] /= 100;
+		}
+	}	
+}
+void FillRand(char arr[STROKA][STOLB], const int STROKA, const int STOLB)
+{	
+	for (int i = 0; i < STROKA; i++)
+	{
+		for (int j = 0; j < STOLB; j++)
+		{
+			arr[i][j]=rand();
+		}
+	}	
+}
+void FillRand(float arr[STROKA][STOLB], const int STROKA, const int STOLB)
+{
+	for (int i = 0; i < STROKA; i++)
+	{
+		for (int j = 0; j < STOLB; j++)
+		{
+			arr[i][j] = rand()%100000;
+			arr[i][j] /= 1000;
+		}
+	}
+}
+void Print(int arr[STROKA][STOLB], const int STROKA, const int STOLB)
+{
+	for (int i = 0; i < STROKA; i++)
+	{
+		for (int j = 0; j < STOLB; j++)
+		{
+			cout << arr[i][j]<<tab;
+		}
+		cout << endl;
+	}
+
+}
+
+void Print(double arr[STROKA][STOLB], const int STROKA, const int STOLB)
+{
+	for (int i = 0; i < STROKA; i++)
+	{
+		for (int j = 0; j < STOLB; j++)
+		{
+			cout << arr[i][j]<<tab;
+		}
+		cout << endl;
+	}
+}
+void Print(char arr[STROKA][STOLB], const int STROKA, const int STOLB)
+{
+	for (int i = 0; i < STROKA; i++)
+	{
+		for (int j = 0; j < STOLB; j++)
+		{
+			cout << arr[i][j]<<tab;
+		}
+		cout << endl;
+	}
+}
+void Print(float arr[STROKA][STOLB], const int STROKA, const int STOLB)
+{
+	for (int i = 0; i < STROKA; i++)
+	{
+		for (int j = 0; j < STOLB; j++)
+		{
+			cout << arr[i][j]<<tab;
+		}
+		cout << endl;
+	}
+}
+double Sum (int arr[STROKA][STOLB], const int STROKA, const int STOLB)
+{
+	int SUM = 0;
+	for (int i = 0; i < STROKA; i++)
+	{
+	    for (int j = 0; j < STOLB; j++)
+		{		  
+		  SUM += arr[i][j];
+		}
+		cout << endl;
+	}
+	return SUM;
+}
+double Sum (double arr[STROKA][STOLB], const int STROKA, const int STOLB)
+{
+	double SUM = 0;
+	for (int i = 0; i < STROKA; i++)
+	{
+	    for (int j = 0; j < STOLB; j++)
+		{		  
+		  SUM += arr[i][j];
+		}
+		cout << endl;
+	}
+	return SUM;
+}
+char Sum(char arr[STROKA][STOLB], const int STROKA, const int STOLB)
+{
+	char SUM = 0;
+	for (int i = 0; i < STROKA; i++)
+	{
+	    for (int j = 0; j < STOLB; j++)
+		{		  
+		  SUM += arr[i][j];
+		}
+		cout << endl;
+	}
+	return SUM;
+}
+float Sum(float arr[STROKA][STOLB], const int STROKA, const int STOLB)
+{
+	float SUM = 0;
+	for (int i = 0; i < STROKA; i++)
+	{
+	    for (int j = 0; j < STOLB; j++)
+		{		  
+		  SUM += arr[i][j];
+		}
+		cout << endl;
+	}
+	return SUM;
+}
+
+double Avg(int arr[STROKA][STOLB], const int STROKA, const int STOLB)
+{
+	return Sum(arr, STROKA, STOLB) / (STROKA * STOLB);
+}
+double Avg(double brr[STROKA][STOLB], const int STROKA, const int STOLB)
+{
+	return Sum(brr, STROKA, STOLB) / (STROKA * STOLB);
+}
+char Avg(char crr[STROKA][STOLB], const int STROKA, const int STOLB)
+{
+	return Sum(crr, STROKA, STOLB) / (STROKA * STOLB);
+}
+float Avg(float drr[STROKA][STOLB], const int STROKA, const int STOLB)
+{
+	return Sum(drr, STROKA, STOLB) / (STROKA * STOLB);
+}
+
+int minValueIn(int arr[STROKA][STOLB], const int STROKA, const int STOLB)
+{
+	int i = 0;
+	int j = 0;
+	int x = 0;	
+	for (int i = 0; i < STROKA; i++)
+	{
+		for ( int j=0; j < STOLB; j++)
+		{
+		    for (int c = 0; c < STROKA; c++)
+			{	
+				for (int d = 0; d < STOLB; d++)
+				{
+					if (arr[i][j] > arr[c][d])
+					{
+						++x;
+					}				
+					
+				}
+			}
+			if (x == 0)
+			{
+				return arr[i][j];
+			}
+			x = 0;
+		}		
+	}	
+}
+double minValueIn(double brr[STROKA][STOLB], const int STROKA, const int STOLB)
+{
+	int i = 0;
+	int j = 0;
+	int x = 0;
+	for (int i = 0; i < STROKA; i++)
+	{		
+		for (int j = 0; j < STOLB; j++)
+		{
+			
+			for (int c = 0; c < STROKA; c++)
+			{				
+				for (int d = 0; d < STOLB; d++)
+				{
+					if (brr[i][j] >= brr[c][d])
+					{
+						++x;	
+					}					
+				}				
+			}	
+			if (x == 0)
+			{
+				return brr[i][j];
+
+			}
+			x = 0;
+		}		
+	}	
+}
+char minValueIn(char crr[STROKA][STOLB], const int STROKA, const int STOLB)
+{
+	int i = 0;
+	int j = 0;
+	int x = 0;
+	for (int i = 0; i < STROKA; i++)
+	{
+		for (int j = 0; j < STOLB; j++)
+		{
+			for (int c = 0; c < STROKA; c++)
+			{
+				for (int d = 0; d < STOLB; d++)
+				{
+					if (crr[i][j] >= crr[c][d])
+					{
+						++x;
+					}
+				}
+			}
+			if (x == 0)
+			{
+				return crr[i][j];
+
+			}
+			x = 0;
+		}
+	}	
+}
+float minValueIn(float drr[STROKA][STOLB], const int STROKA, const int STOLB)
+{
+	int i = 0;
+	int j = 0;
+	int x=0;
+	for (int i = 0; i < STROKA; i++)
+	{
+		for (int j = 0; j < STOLB; j++)
+		{
+			for (int c = 0; c < STROKA; c++)
+			{
+				for (int d = 0; d < STOLB; d++)
+				{
+					if (drr[i][j] > drr[c][d])
+					{
+						++x;
+					}					
+				}
+			}
+			if (x == 0)
+			{
+				return drr[i][j];
+
+			}
+			x = 0;
+		}
+	}	
+}
+int maxValueIn(int arr[STROKA][STOLB], const int STROKA, const int STOLB)
+{
+	int i = 0;
+	int j = 0;
+	int x = 0;
+	for (int i = 0; i < STROKA; i++)
+	{
+		for (int j = 0; j < STOLB; j++)
+		{
+			for (int c = 0; c < STROKA; c++)
+			{
+				for (int d = 0; d < STOLB; d++)
+				{
+					if (arr[i][j] < arr[c][d])
+					{
+						++x;
+					}
+
+				}
+			}
+			if (x == 0)
+			{
+				return arr[i][j];
+			}
+			x = 0;
+		}
+	}
+
+}
+double maxValueIn(double brr[STROKA][STOLB], const int STROKA, const int STOLB)
+{
+	int i = 0;
+	int j = 0;
+	int x = 0;
+	for (int i = 0; i < STROKA; i++)
+	{
+		for (int j = 0; j < STOLB; j++)
+		{
+
+			for (int c = 0; c < STROKA; c++)
+			{
+				for (int d = 0; d < STOLB; d++)
+				{
+					if (brr[i][j] < brr[c][d])
+					{
+						++x;
+
+					}
+				}
+			}
+			if (x == 0)
+			{
+				return brr[i][j];
+
+			}
+			x = 0;
+		}
+	}
+}
+char maxValueIn(char crr[STROKA][STOLB], const int STROKA, const int STOLB)
+{
+	int i = 0;
+	int j = 0;
+	int x = 0;
+	for (int i = 0; i < STROKA; i++)
+	{
+		for (int j = 0; j < STOLB; j++)
+		{
+			for (int c = 0; c < STROKA; c++)
+			{
+				for (int d = 0; d < STOLB; d++)
+				{
+					if (crr[i][j] < crr[c][d])
+					{
+						++x;
+					}
+				}
+			}
+			if (x == 0)
+			{
+				return crr[i][j];
+
+			}
+			x = 0;
+		}
+	}
+}
+float maxValueIn(float drr[STROKA][STOLB], const int STROKA, const int STOLB)
+{
+	int i = 0;
+	int j = 0;
+	int x = 0;
+	for (int i = 0; i < STROKA; i++)
+	{
+		for (int j = 0; j < STOLB; j++)
+		{
+			for (int c = 0; c < STROKA; c++)
+			{
+				for (int d = 0; d < STOLB; d++)
+				{
+					if (drr[i][j] < drr[c][d])
+					{
+						++x;
+					}
+				}
+			}
+			if (x == 0)
+			{
+				return drr[i][j];
+
+			}
+			x = 0;
+		}
+	}
+}
+/*void ShiftLeft(int arr[STROKA][STOLB], const int STROKA, const int STOLB)
+{
+	int y;
+	cin >> y;
+	for (int i = 0; i < y; i++)
+	{
+		int x = arr[0][0];
+		for (int i = 0; i < STROKA; i++)
+		{
+			for(int j = 0; j <STOLB; j++)
+			arr[i][j] = arr[i][j+1];
+	    }
+		arr[STROKA-1][STOLB-2] = x;
+	}
+	cout << endl;
+
+}*/
